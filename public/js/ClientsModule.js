1a579360d8d6af1313c0ee8cad6201fd948d1172
Gabriel Camacho <kbo025@gmail.com>
2017-05-29 10:49:49 -0400

    % haciendo bonito ClientsModule

--- public/js/ClientsModule.js
+++ public/js/ClientsModule.js
@@ -3,7 +3,6 @@ Ext.require([
     'Ext.data.*',
     'Ext.panel.*',
     'Ext.util.*',
-    //'Ext.field.Select',
     'Ext.layout.container.Border'
 ]);
 
@@ -21,7 +20,27 @@ Ext.onReady(function(){
             {xtype : "textfield", name : "id", hidden: true, allowBlank:true},
 			{xtype : "textfield", name : "identification", fieldLabel : "Identification"},
 			{xtype : "textfield", name : "name", fieldLabel : "Name"},
-			//{xtype : "selectfield", name : "type", fieldLabel : "Type",options:[{text:'Client',value:'client'},{text:'Provider',value:'provider'}]},
+			{
+                xtype       : "fieldcontainer",
+                name        : "type",
+                fieldLabel  : "Type",
+                defaultType : 'radiofield',
+                defaults    : {flex: 1},
+                layout      : 'hbox',
+                items       : [
+                    {
+                        boxLabel  : 'Client',
+                        name      : 'type',
+                        inputValue: 'client',
+                        id        : 'radio1'
+                    }, {
+                        boxLabel  : 'Provider',
+                        name      : 'type',
+                        inputValue: 'provider',
+                        id        : 'radio2'
+                    }
+                ]
+            },
             {xtype : "textfield", name : "email", fieldLabel : "E-mail", vtype :"email"},
             {xtype : "textfield", name : "phonePrimary", fieldLabel : "Primary Phone", regexp: '/\d/i', invalidText: 'Not a valid value.'},
             {xtype : "textfield", name : 'phoneSecondary', fieldLabel : "Secondary Phone", allowBlank:true, regexp: '/\d/i', invalidText: 'Not a valid value.'},
@@ -32,9 +51,9 @@ Ext.onReady(function(){
 		],
 	    fbar		: [
             {
-                text : "save",
-                scope : this,
-                handler: function() {
+                text        : "save",
+                scope       : this,
+                handler     : function() {
                     if (!form.isValid()) {
                         Ext.Msg.alert("Error","Campos Incorrectos!!!");
                         return false;
@@ -45,17 +64,18 @@ Ext.onReady(function(){
                     } else {
                         let contact = Ext.create('Client',{
 			                identification	: form.getForm().getValues().identification,
-			                name	: form.getForm().getValues().name,
-			                //type	: form.getForm().getValues().type,
-			                email	: form.getForm().getValues().email,
+			                name	        : form.getForm().getValues().name,
+			                type	        : form.getForm().getValues().type,
+			                email	        : form.getForm().getValues().email,
                             phonePrimary	: form.getForm().getValues().phonePrimary,
                             phoneSecondary	: form.getForm().getValues().phoneSecondary,
-                            fax	: form.getForm().getValues().fax,
-                            mobile	: form.getForm().getValues().mobile,
-                            address	: form.getForm().getValues().address,
+                            fax	            : form.getForm().getValues().fax,
+                            mobile	        : form.getForm().getValues().mobile,
+                            address	        : form.getForm().getValues().address,
                             observations	: form.getForm().getValues().observations,
 		                });
                         store.insert(0,contact);
+                        store.reload();
                     }
                     form.reset(true);
                     win.hide();
@@ -65,41 +85,37 @@ Ext.onReady(function(){
     });
 
     var win = Ext.create('Ext.window.Window',{
-        title: 'Client',
-        width: 400,
-        modal: true,
-        closeAction: 'method-hide',
-        items: [form]
+        title       : 'Client',
+        width       : 400,
+        modal       : true,
+        closeAction : 'method-hide',
+        items       : [form]
+    });
+
+    var proxy = Ext.create('Ext.data.proxy.Ajax',{
+        api     : {
+            read 	: "search",
+            create 	: "save",
+            update	: "save",
+            destroy	: "delete"
+        },
+        reader  : {
+            type            : 'json',
+            totalProperty   : 'total',
+            rootProperty    : 'items',
+        },
+        writer  : {
+            type            : 'json',
+            writeAllFields  : true
+        },
+        filterParam: 'query'
     });
 
     Ext.define('Client',{
-        extend: 'Ext.data.Model',
-        idProperty: 'post_id',
-        proxy: {
-            // load using HTTP
-            type: 'ajax',
-            api: {
-                read 	: "testclients",
-                create 	: "savetest",
-                update	: "savetest",
-                destroy	: "deletetest"
-            },
-            // the return will be XML, so lets set up a reader
-            reader: {
-                type: 'json',
-                totalProperty  : 'total',
-                rootProperty: 'items',
-            },
-            writer: {
-                type: 'json',
-                writeAllFields: true
-            }
-            //simpleSortMode: true,
-            //filterParam: 'query'
-        },
+        extend      : 'Ext.data.Model',
+        idProperty  : 'post_id',
+        proxy       : proxy,
         fields: [
-            // set up the fields mapping into the xml doc
-            // The first needs mapping, the others are very basic
             'id',
             'name',
             'identification',
@@ -118,36 +134,9 @@ Ext.onReady(function(){
         ]
     });
 
-    // create the Data Store
     var store = Ext.create('Ext.data.Store', {
         model   : 'Client',
-        //pageSize: 50,
-        //leadingBufferZone: 1000,
-        proxy   : {
-            // load using HTTP
-            type: 'ajax',
-            api: {
-                read 	: "testclients",
-                create 	: "savetest",
-                update	: "savetest",
-                destroy	: "deletetest"
-            },
-            // the return will be XML, so lets set up a reader
-            reader: {
-                type: 'json',
-                totalProperty  : 'total',
-                rootProperty: 'items',
-            },
-            writer: {
-                type: 'json',
-                writeAllFields: true
-            },
-            simpleSortMode: true,
-            filterParam: 'query'
-        },
-        /*listeners: {
-            totalcountchange: onStoreSizeChange
-        },*/
+        proxy   : proxy,
         remoteFilter: true,
         autoLoad: true,
         autoSave: true,
@@ -155,43 +144,29 @@ Ext.onReady(function(){
     });
 
     Ext.override(store.getProxy(), {
-        applyEncoding: function(a) {
+        applyEncoding   : function(a) {
             return a;
         }
     });
 
-    /*function onStoreSizeChange() {
-        grid.down('#status').update({count: store.getTotalCount()});
-    }*/
-
-    /*function renderTopic(value, p, record) {
-        return Ext.String.format(
-            '<a href="http://sencha.com/forum/showthread.php?p={1}" target="_blank">{0}</a>',
-            value,
-            record.getId()
-        );
-    }*/
-
-    // create the grid
     var grid = Ext.create('Ext.grid.Panel', {
-        //bufferedRenderer: false,
-        store: store,
-        forceFit: true,
-        width: 600,
-        split: true,
-        region: 'west',
-        collapsible: true,
-        remoteSort: true,
-        columns: [
+        store       : store,
+        forceFit    : true,
+        width       : 600,
+        split       : true,
+        region      : 'west',
+        collapsible : true,
+        remoteSort  : true,
+        columns     : [
             {text: "Identification", width: 120, dataIndex: 'identification', sortable: true},
             {
-                text: "Name",
-                flex: 1,
-                dataIndex: 'name',
-                sortable: true,
-                filter: {
-                    type: 'string',
-                    serializer: function(filter) {
+                text        : "Name",
+                flex        : 1,
+                dataIndex   : 'name',
+                sortable    : true,
+                filter      : {
+                    type        : 'string',
+                    serializer  : function(filter) {
                         return filter.value;
                     }
                 }
@@ -199,33 +174,32 @@ Ext.onReady(function(){
             {text: "email", width: 125, dataIndex: 'email', sortable: true},
             {text: "Phone", width: 125, dataIndex: 'phonePrimary', sortable: true}
         ],
-
-        tbar: [
+        tbar        : [
             {
-                text: "New",
-                scope: this,
-                handler: function() {
+                text    : "New",
+                scope   : this,
+                handler : function() {
                     form.reset(true);
                     win.show();
                 },
-                iconCls:'delete-icon'
+                iconCls :'delete-icon'
             },
             {
-                text:"Update",
-                scope:this,
-                handler: function() {
+                text    :"Update",
+                scope   :this,
+                handler : function() {
                     var rows = grid.getSelectionModel().getSelection();
                     if(rows.length === 0){
                         return false;
                     }
                     win.show();
                 },
-                iconCls:'delete-icon'
+                iconCls :'delete-icon'
             },
 			{
-                text:"Delete",
-                scope:this,
-                handler: function() {
+                text    :"Delete",
+                scope   :this,
+                handler : function() {
                     var rows = grid.getSelectionModel().getSelection();
                     if(rows.length === 0){
                         return false;
@@ -241,12 +215,12 @@ Ext.onReady(function(){
                         }
                     );
                 },
-                iconCls:'delete-icon'
+                iconCls :'delete-icon'
             }
 		],
-        stripeRows: true,
-        loadMask: true,
-        dockedItems: [{
+        stripeRows  : true,
+        loadMask    : true,
+        dockedItems : [{
             dock: 'top',
             xtype: 'toolbar',
             items: ['->', {
@@ -256,7 +230,7 @@ Ext.onReady(function(){
                 style: 'margin-right:5px'
             }]
         }],
-        plugins: 'gridfilters',
+        plugins     : 'gridfilters',
     });
         
     var tplMarkup = [
@@ -269,23 +243,24 @@ Ext.onReady(function(){
     var tpl = Ext.create('Ext.Template', tplMarkup);
 
     Ext.create('Ext.Panel', {
-        renderTo: 'binding-example',
-        frame: true,
-        title: 'Client List',
-        width: 1040,
-        height: 600,
-        layout: 'border',
-        items: [
-            grid, {
-                id: 'detailPanel',
-                region: 'center',
-                bodyPadding: 7,
-                bodyStyle: "background: #ffffff;",
-                html: 'Please select a book to see additional details.'
-        }]
+        renderTo    : 'binding-example',
+        frame       : true,
+        title       : 'Client List',
+        width       : 1040,
+        height      : 600,
+        layout      : 'border',
+        items       : [
+            grid, 
+            {
+                id          : 'detailPanel',
+                region      : 'center',
+                bodyPadding : 7,
+                bodyStyle   : "background: #ffffff;",
+                html        : 'Please select a client to see additional details.'
+            }
+        ]
     });
     
-    // update panel body on selection change
     grid.getSelectionModel().on('selectionchange', function(sm, selectedRecord) {
         if (selectedRecord.length) {
             var detailPanel = Ext.getCmp('detailPanel');
